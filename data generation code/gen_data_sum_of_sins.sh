406164a8b43ec9cbe6d68af04f3604c1d5485817
Davide Pillon <davide.pillon04@gmail.com>
2026-06-06 12:32:57 +0200

    Initial commit

---
+++ data generation code/gen_data_sum_of_sins.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Giulio Giacomuzzo 20/08/2022
+    # This script generates trajectories of the PANDA robot with increasing dregrees of freedom (from 3 to 7)
+    # Trajectories contains joint position, velocities, accelerations and torques,
+    # inertia matrices and potential energy (kinetic energy is easily computed from velocities and inertia matrices)
+    # seed 0 is intended for training, other seeds for test.
+    # By default the number of testing seed is 50 [1..50], but it can be passed as an argument
+    # @params: 
+        # - (int) num_seeds : number of test trajectories to be generated
+
+if [ $# = 1 ]
+then
+    num_seed=$1
+else
+    num_seed=50
+fi
+
+if [ ! -d 'data/' ] 
+then
+    echo "Directory 'data/' does not exists. Creating it."
+    mkdir -p './data/'
+fi
+for i in {2..7}
+do 
+    echo 'Generating MC data: '$i$'dof'
+    if [ ! -d './data/'$i$'dof' ] 
+    then
+        echo " Directory 'data/'$i$'dof' does not exists. Creating it."
+        mkdir -p './data/'$i$'dof'
+    fi
+    saving_path='./data/'$i$'dof/'
+    #echo $saving_path
+    #for (( seed=0; seed<=$num_seed; seed++ ))
+    #do
+        #echo ' seed: '$seed$' 50 sinusoids'
+        #python Get_PANDA_data_inv_dyn_sum_of_sin.py -saving_path $saving_path -pos_range 0.8 -c_init 0.1 -num_sin 50 -seed $seed -std_noise 0.01 -sampling_time 0.01 -num_active_joints $i -plot 0
+        
+    #done
+
+    for (( seed=100; seed<=100+$num_seed; seed++ ))
+    do 
+        echo ' seed: '$seed$' 50 sinusoids'
+        python Get_PANDA_data_inv_dyn_sum_of_sin.py -saving_path $saving_path -pos_range 1.2 -c_init 0.1 -num_sin 30 -seed $seed -std_noise 0.01 -sampling_time 0.01 -num_active_joints $i -plot 0
+        
+    done
+
+done
+
+
+# for i in range(3,8):
+#     if not os.path.exists('data/MC_test/data/'+str(i)+'dof'):
+#         os.mkdir('data/MC_test/data/'+str(i)+'dof')
+
+
+
+
+
+
+
+
+# # 'python Get_PANDA_data_inv_dyn.py -saving_path ' + saving_path + ' -pos_range 0.5' + ' -seed '+ str(seed) + ' -std_noise 0.01' + ' num_active_joints ' + str(i) + '-flg_plot False'
+
+
+# if [ $# = 1 ]
+# then
+#     num_seed = $1
+# #     for i in {0..num_seed}
+# #     do 
+# #         python Get_PANDA_data_inv_dyn.py -seed $i -saving_path 'data/data_no_noise/'
+# #     done
+
+# # fi
